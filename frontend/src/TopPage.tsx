3dccdb86fcfd6d052e5459f7858341975e2a03d0
KaiYamashita <kai-yama12345679@g.ecc.u-tokyo.ac.jp>
2022-11-21 20:22:52 +0900

    update

--- frontend/src/TopPage.tsx
+++ frontend/src/TopPage.tsx
@@ -14,6 +14,7 @@ import { onAuthStateChanged } from "firebase/auth";
 import { fireAuth } from "./firebase";
 import MDSpinner from "react-md-spinner";
 import { LoginForm } from "./LoginForm";
+import Avatar from '@mui/material/Avatar';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -51,6 +52,35 @@ export const options = {
 
 type Props = {
     Info: userInfo[]
+}
+
+function stringToColor(string: string) {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = '#';
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
+
+function stringAvatar(name: string) {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+  };
 }
 export const UserId = createContext("");
 const TopPage = (props: Props) => {
@@ -98,7 +128,7 @@ const TopPage = (props: Props) => {
           <div>
             <LoginForm />
           <Link to="/user" state={{id: loginUser}}>ユーザーページへ</Link>
-          <img src={loginUser.photoURL} alt="" />
+          <Avatar  src={loginUser.photoURL}/>
           </div>
         ) 
       } else {
@@ -106,6 +136,7 @@ const TopPage = (props: Props) => {
           <div>
             <LoginForm />
           <Link to="/user" state={{id: loginUser}}>ユーザーページへ</Link>
+          <Avatar {...stringAvatar(loginUser.displayName?loginUser.displayName:"")} />
           </div>
         )
       }
